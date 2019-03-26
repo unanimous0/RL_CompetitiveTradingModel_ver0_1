a0055f5be19ab2422e8e3187816ea1cf3c87fe17
Vlack_Env <unanimous0@kaist.ac.kr>
2019-03-26 14:13:48 -0400

    no message

---
+++ Project/new.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from Project.Webreader2 import Webreader2
+from Project.Kiwoom import Kiwoom
+import sqlite3
+from datetime import datetime, timedelta
+import pandas as pd
+from pandas import DataFrame
+
+con = sqlite3.connect("c:/Users/Eugene/pyquery.db")
+cursor = con.cursor()
+sqldhfjsdjfafjdsjfjkjfds
